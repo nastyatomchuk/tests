ae4cc6fcff590afb0b6d874b54daa64969459031
NastiushaTom <78820851+NastiushaTom@users.noreply.github.com>
2021-12-02 20:57:44 +0300

    fix

--- spec/registry/registry_spec.rb
+++ spec/registry/registry_spec.rb
@@ -1,68 +1,53 @@
 require 'rspec'
-require_relative '../../spec/spec_helper'
-# require 'spec/spec_helper'
-require_relative '../../lib/registry/registry'
+require 'spec_helper'
+require 'registry/registry'
 
 describe DuplicateFilesRegistry do
-  let(:registry) { DuplicateFilesRegistry.new }
 
   before do
-    data_path = File.expand_path('../../data/', __dir__)
-    digester = Sha1Digester.new
-    file_names = Dir.entries(data_path).select {|f| !File.directory? f}
-
-    file_names.each  do |file_name|
-      full_file_name = File.join( data_path , file_name)
-      digest = digester.digest(full_file_name)
-      registry.add_file(digest, full_file_name)
-    end
+    @registry = DuplicateFilesRegistry.new
   end
 
   context "digests" do
-    it "creates digest array" do
-      expect(registry.digests).to eql(["ecfacd866c94ed69bf8d810d70445adfa82858d2",
-                                       "c815eaafda365cf8b805fed05d6ccb04bddca917",
-                                       "341170e3a4c2e5f0e1587de9d94e87be77c76ee8",
-                                       "0281e9239cc465e66b197c71af1c46c116360d20"])
+    let(:empty_array) { [] }
+
+    it "creates empty digest array" do
+      expect(@registry.digests).to eql(empty_array)
     end
   end
 
   context "grouped_files" do
-    it "groups files into arrays by keysy" do
-      expect(registry.grouped_files).to eql([["C:/Users/Sad_7400/apps/tests/data/2.jpg"],
-                                             ["C:/Users/Sad_7400/apps/tests/data/22.jpg", "C:/Users/Sad_7400/apps/tests/data/222.jpg", "C:/Users/Sad_7400/apps/tests/data/23.jpg"],
-                                             ["C:/Users/Sad_7400/apps/tests/data/3.jpg", "C:/Users/Sad_7400/apps/tests/data/34.jpg"],
-                                             ["C:/Users/Sad_7400/apps/tests/data/4.jpg"]])
-    end
-  end
+    let(:empty_array) { [] }
 
-  context "grouped_files(digest)" do
-    let(:digest) { "c815eaafda365cf8b805fed05d6ccb04bddca917" }
-    xit "outputs files with the corresponding digest" do
-      expect(registry.group_by(digest)).to raise_error
-    end
-  end
-
-  context "each" do
-    it "displays digests and files that correspond to them" do
-      expect(registry.each{ |duplicates| puts duplicates }).to eql({"ecfacd866c94ed69bf8d810d70445adfa82858d2"=>["C:/Users/Sad_7400/apps/tests/data/2.jpg"],
-                                                                    "c815eaafda365cf8b805fed05d6ccb04bddca917"=>["C:/Users/Sad_7400/apps/tests/data/22.jpg", "C:/Users/Sad_7400/apps/tests/data/222.jpg", "C:/Users/Sad_7400/apps/tests/data/23.jpg"],
-                                                                    "341170e3a4c2e5f0e1587de9d94e87be77c76ee8"=>["C:/Users/Sad_7400/apps/tests/data/3.jpg", "C:/Users/Sad_7400/apps/tests/data/34.jpg"],
-                                                                    "0281e9239cc465e66b197c71af1c46c116360d20"=>["C:/Users/Sad_7400/apps/tests/data/4.jpg"]})
+    it "returns empty array" do
+      expect(@registry.grouped_files).to eql(empty_array)
     end
   end
 
   context "empty?" do
-    let(:result) { false }
-    it "checks if case is empty" do
-      expect(registry.empty?).to eql(result)
+    let(:empty) { true }
+
+    it "returns that registry is empty" do
+      expect(@registry.empty?).to eql(empty)
     end
   end
 
   context "uniq_files_count" do
-    let(:uniq_files_count) { 4 }
-    it "counts the number of unique filesy" do
-      expect(registry.uniq_files_count).to eql(uniq_files_count)
+    let(:uniq_files_count) { 0 }
+
+    it "returns zero" do
+      expect(@registry.uniq_files_count).to eql(uniq_files_count)
+    end
+  end
+
+  context "add file" do
+    let(:data_path ) { 'data/22.jpg' }
+    let(:digest ) { 'c815eaafda365cf8b805fed05d6ccb04bddca917' }
+    let(:grouped_files) { [[data_path]] }
+
+    it "add file in empty registry" do
+      @registry.add_file(digest, data_path)
+      expect(@registry.grouped_files).to eql(grouped_files)
     end
   end
 end
