9b253ba0379bd997d08f5725ad76af2efaa437d8
NastiushaTom <78820851+NastiushaTom@users.noreply.github.com>
2021-12-02 20:59:36 +0300

    fix

--- spec/users/user_spec.rb
+++ spec/users/user_spec.rb
@@ -1,32 +1,28 @@
 require 'rspec'
-
-require_relative '../../spec/spec_helper'
-
-# require './spec/spec_helper'ls
-require_relative '../../lib/users/user'
-
+require 'spec_helper'
+require 'users/user'
 
 describe User do
 
-  context "new user" do
+  context "full_name" do
     let (:full_name){ 'Ivan Petroff' }
     let(:user) { User.new }
 
-    it "makes a user with name Ivan Petrov" do
+    it "returns correct value when no name provided" do
       expect(user.full_name).to eql(full_name)
     end
   end
 
-  context "new user with full name" do
+  context "full_name" do
     let (:full_name){ 'Ivan Ivanov' }
     let(:user) { User.new('Ivan', 'Ivanov') }
 
-    it "makes a user with name Ivan Ivanov" do
+    it "returns correct value when all names provided" do
       expect(user.full_name).to eql(full_name)
     end
   end
 
-  context "full name is string" do
+  context "full_name" do
     let(:user) { User.new('Ivan', 'Ivanov') }
 
     it "returns string type" do
@@ -34,48 +30,46 @@ describe User do
     end
   end
 
-  context "default user first name " do
+  context "full_name" do
+    let (:full_name){ 'Petr Petroff' }
+    let(:user) { User.new('Petr') }
+
+    it "returns correct value when only first name provided" do
+      expect(user.full_name).to eql(full_name)
+    end
+  end
+
+  context "full_name" do
     let (:first_name){ 'Ivan' }
-    let(:user) { User.new }
+    let(:user) { User.new('Ivan', '') }
 
-    it "returns default user first name" do
-      expect(user.first_name).to eql(first_name)
+    it "returns correct value when empty last name provided" do
+      expect(user.full_name).to eql(first_name)
     end
   end
 
-  context "default user last name " do
-    let (:first_name){ 'Petroff' }
-    let(:user) { User.new }
+  context "full_name" do
+    let (:first_name){ 'Ivan' }
+    let(:user) { User.new('Ivan', nil) }
 
-    it "returns default user last name" do
-      expect(user.last_name).to eql(first_name)
-    end
-  end
-
-  context "user last name " do
-    let (:first_name){ 'Petya' }
-    let(:user) { User.new('Petya', 'Petroff') }
-
-    it "returns user last name" do
-      expect(user.first_name).to eql(first_name)
+    it "returns correct value when nil last name provided" do
+      expect(user.full_name).to eql(first_name)
     end
   end
 
   context "user last name " do
-    let (:last_name){ 'Petroff' }
-    let(:user) { User.new('Petya', 'Petroff') }
+    let(:user) { User.new('', '') }
 
-    it "returns user last name" do
-      expect(user.last_name).to eql(last_name)
+    it "raises argument error when empty names provided" do
+      expect{ user.full_name }.to raise_error(ArgumentError)
     end
   end
 
-  context "full name length" do
-    let (:count){ 12 }
-    let(:user) { User.new('Petya', 'Petroff') }
+  context "user last name " do
+    let(:user) { User.new('', '') }
 
-    it "returns the number of characters in the full name " do
-      expect(user.full_name.length - 1).to eql(count)
+    it "raises argument error when nil names provided" do
+      expect{ user.full_name }.to raise_error(ArgumentError)
     end
   end
 end
