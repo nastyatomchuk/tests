903b45fea364dade28de5332f0e2db6f87aa822b
NastiushaTom <78820851+NastiushaTom@users.noreply.github.com>
2021-12-06 13:24:11 +0300

    add minitest-reporters

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,1 +1,5 @@
+require 'bundler/setup'
 require 'minitest/autorun'
+require 'minitest/pride'
+require "minitest/reporters"
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(:color => true)]
