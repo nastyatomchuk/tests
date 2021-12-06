409b450346b841dd59df18bf09c0ed4258a94423
Anton Ageev <anthony.ageev@gmail.com>
2021-12-06 13:47:04 +0300

    fixes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-require 'rspec'
-=======
-require 'bundler/setup'
 require 'minitest/autorun'
 require 'minitest/pride'
 require "minitest/reporters"
 Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(:color => true)]
->>>>>>> main
--- test/users/user_test.rb
+++ test/users/user_test.rb
@@ -1,47 +1,3 @@
-<<<<<<< HEAD
-require 'bundler/setup'
-require 'minitest/autorun'
-require_relative '../../lib/users/user'
-
-class UserTest < Minitest::Test
-
-  def test_positive_full_name_initialize
-     assert_equal('Ivan Petroff', User.new.full_name)
-  end
-
-  def test_positive_full_name
-    assert_equal('Ivan Ivanov', User.new('Ivan', 'Ivanov').full_name)
-  end
-
-  def test_positive_full_name_string
-    assert_instance_of(String, User.new('Ivan', 'Ivanov').full_name)
-  end
-
-  def test_default_first_name
-    user = User.new()
-    assert_equal("Ivan", user.first_name)
-  end
-
-  def test_default_last_name
-    user = User.new()
-    assert_equal("Petroff", user.last_name)
-  end
-
-  def test_first_name
-    user = User.new('Petya', 'Petroff')
-    assert_equal("Petya", user.first_name)
-  end
-
-  def test_last_name
-    user = User.new('Petya', 'Petroff')
-    assert_equal("Petroff", user.last_name)
-  end
-
-  def test_length_full_name
-    assert_equal(10, User.new('Ivan', 'Ivanov').full_name.length - 1)
-  end
-end
-=======
 require 'test_helper'
 require 'users/user'
 
@@ -92,4 +48,3 @@ class UserTest < Minitest::Test
     assert_raises(ArgumentError) { User.new(nil, 'Petrov') }
   end
 end
->>>>>>> main
